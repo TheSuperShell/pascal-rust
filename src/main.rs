f5f30f8fb33b74062c8c33f0e842cafc9107550d
Steven Rudenko <steven.gucum@gmail.com>
2026-02-12 02:05:52 +0300

    add cli

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-use dotenv::dotenv;
+use clap::{arg, command};
 use pascal_rust::interprete;
 use tracing::error;
 use tracing_subscriber::{EnvFilter, fmt};
 
-pub fn init_logging() {
-    dotenv().ok();
-    let filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("pascal::semantic=info,pascal::interp=warn"));
+pub fn init_logging(stack: bool, scope: bool) {
+    let directives = format!(
+        "pascal::semantic={},pascal::interp={}",
+        match scope {
+            true => "debug",
+            false => "warn",
+        },
+        match stack {
+            true => "debug",
+            false => "warn",
+        }
+    );
+    let filter = EnvFilter::new(directives);
     fmt().with_env_filter(filter).compact().init();
 }
 
 fn main() {
-    init_logging();
-    match interprete("examples/factorial.pas") {
+    let matches = command!()
+        .arg(arg!(<path> "Path of the scrip"))
+        .arg(arg!(--scope "Turn on scope logging"))
+        .arg(arg!(--stack "Turn on stack logging"))
+        .get_matches();
+    let path = matches.get_one::<String>("path").unwrap();
+    init_logging(matches.get_flag("stack"), matches.get_flag("scope"));
+    match interprete(path) {
         Err(e) => error!("{e}"),
         _ => (),
     }
