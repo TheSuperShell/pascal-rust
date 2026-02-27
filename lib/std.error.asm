9951a2d940198f67ad2a48d7efbbdda1b18ea293
Steven Rudenko <steven.gucum@gmail.com>
2026-02-27 21:16:35 +0300

    refactor: update division by zero error handling to use WriteFile instead of puts

--- lib/std.error.asm
+++ lib/std.error.asm
@@ -1,17 +1,32 @@
 default rel
 
 section .rdata
-div0_msg db "Runtime error: division by zero", 0
+div0_msg db "Runtime error: division by zero"
+div0_len equ $ - div0_msg
 
 section .text
 global std.error.div0_error
-extern puts
+
+extern GetStdHandle
+extern WriteFile
 extern ExitProcess
 
+STD_ERROR_HANDLE equ -12
+
 std.error.div0_error:
-    sub rsp, 32
-    lea rcx, [div0_msg]
-    call puts
-    add rsp, 32
+    sub rsp, 72
+    
+    mov ecx, STD_ERROR_HANDLE
+    call GetStdHandle
+
+    mov rcx, rax
+    lea rdx, [div0_msg]
+    mov r8d, div0_len
+    lea r9, [rsp + 40]
+
+    mov qword [rsp + 32], 0
+    call WriteFile
+
+    add rsp, 72
     mov ecx, 1
     call ExitProcess
